2c392b1ef6539acc9fd607779a33675c07a14991
Orv <pouria.orv@gmail.com>
2025-01-27 17:17:08 +0330

    fix logger

--- src/logger.py
+++ src/logger.py
@@ -3,9 +3,10 @@ from config import LOG_FILE
 
 def setup_logger():
     logger = logging.getLogger("TelegramUserSaver")
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(LOG_FILE)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(LOG_FILE)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
